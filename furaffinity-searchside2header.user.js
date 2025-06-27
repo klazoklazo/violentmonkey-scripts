917c21ac647335a1afc2ad6b7b043239d91e5fa1
klazo <153469868+klazoklazo@users.noreply.github.com>
2025-06-27 06:02:50 +0000

    update description in furaffinity-searchside2header.user.js

--- furaffinity-searchside2header.user.js
+++ furaffinity-searchside2header.user.js
@@ -5,7 +5,7 @@
 // @grant       none
 // @version     1.0
 // @author      klazo
-// @description move the sidebar to the top so theres less negative space
+// @description move the search pages sidebar to the top so theres less negative space
 // ==/UserScript==
 
 document.querySelector("#search-form").appendChild(document.querySelector(".browse-content"));
