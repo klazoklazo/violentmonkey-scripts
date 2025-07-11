c492495184af68da6fc56db03a0ec7a83e825b46
klazo <153469868+klazoklazo@users.noreply.github.com>
2025-07-11 03:23:36 +0000

    Update furaffinity-contactfix.user.js
    fixed minor bug where an error would show up in the console if a userpage didnt have any contacts

--- furaffinity-contactfix.user.js
+++ furaffinity-contactfix.user.js
@@ -3,36 +3,39 @@
 // @namespace   Violentmonkey Scripts
 // @match       *://www.furaffinity.net/user/*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      klazo
 // @description sometimes a furaffinity user will include the domain of a site within their contacts, resulting in a duplicated domain within the full url. this fixes that by removing that second domain
 // ==/UserScript==
 
-// thank you past me for making this with redirector before
-// regex to match and split apart urls with duplicated domains
-const contactCheck = /^(https:\/\/)([^/]*\/)(https?%3A%2F%2F)?([^.]*)(\.com|\.app|\.tv)(%2F)(.*)/gi;
+// first check if the userpage has contacts or not
+if (!!document.getElementById("userpage-contact")) {
+  // thank you past me for making this with redirector before
+  // regex to match and split apart urls with duplicated domains
+  const contactCheck = /^(https:\/\/)([^/]*\/)(https?%3A%2F%2F)?([^.]*)(\.com|\.app|\.tv)(%2F)(.*)/gi;
 
-// gather all the contact links within a userpage
-const contactElements = Array.from(document.getElementById("userpage-contact").getElementsByTagName("a"));
+  // gather all the contact links within a userpage
+  const contactElements = Array.from(document.getElementById("userpage-contact").getElementsByTagName("a"));
 
-// go through all the contact links to check and modify as needed
-contactElements.forEach(function(contactElement) {
-  let contactLink = contactElement.href;
+  // go through all the contact links to check and modify as needed
+  contactElements.forEach(function(contactElement) {
+    let contactLink = contactElement.href;
 
-  // i hate the fact i have to create this regex object every time
-  // but writing 0 into lastIndex still produces weird ass behavior
-  if (contactCheck.test(contactLink)) {
-    // reset regex objects lastIndex value
-    contactCheck.lastIndex = 0;
+    // i hate the fact i have to create this regex object every time
+    // but writing 0 into lastIndex still produces weird ass behavior
+    if (contactCheck.test(contactLink)) {
+      // reset regex objects lastIndex value
+      contactCheck.lastIndex = 0;
 
-    // split apart and reassemble broken url into a working url
-    let contactDuplicates = contactCheck.exec(contactLink); contactCheck.lastIndex = 0;
-    let contactDomain = contactDuplicates[2];
-    let contactUsername = contactDuplicates[7];
-    let contactLinkNew = "https://" + contactDomain + contactUsername;
+      // split apart and reassemble broken url into a working url
+      let contactDuplicates = contactCheck.exec(contactLink); contactCheck.lastIndex = 0;
+      let contactDomain = contactDuplicates[2];
+      let contactUsername = contactDuplicates[7];
+      let contactLinkNew = "https://" + contactDomain + contactUsername;
 
-    // replace href and textContent with working counterparts
-    contactElement.href = contactLinkNew;
-    contactElement.textContent = contactUsername;
-  }
-});
+      // replace href and textContent with working counterparts
+      contactElement.href = contactLinkNew;
+      contactElement.textContent = contactUsername;
+    }
+  });
+}
